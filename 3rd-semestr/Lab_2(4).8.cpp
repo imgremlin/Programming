55cd22e80586a7a0fa8ee6a3da408b205c267a48
Alex <alextsepa001@gmail.com>
2019-10-02 09:06:20 +0300

    Update Lab_2(4).8.cpp

--- 3rd-semestr/Lab_2(4).8.cpp
+++ 3rd-semestr/Lab_2(4).8.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>   
+#include <algorithm>
 
 using namespace std; 
 
@@ -108,8 +109,10 @@ int main()
 	cout<<"How many objects u wanna add: ";
 	cin>>count;
 	cout<<endl;
+	
 	for(int i=0; i<count; i++)
 	{
+		bool check = true;
 		cout<<"Input Number "<<i+1<<endl;
 		cout<<"Input name: ";
 		cin>>nm;
@@ -123,10 +126,14 @@ int main()
 		while (1)
 		{
 			cin>>y;
-			if (y<=g1[g1.size()-1].get_year())
-				cout<<"Incorrect year, input 1 more time: ";
-			else
+			if (y<g1[g1.size()-1].get_year())
+				cout<<"Incorrect year, after: "<<g1[g1.size()-1].get_year()<< " ";
+			else if (y==g1[g1.size()-1].get_year() )
+			{
+				check = false;
 				break;
+			}
+			else break;	
 			}
 		
 		
@@ -134,10 +141,16 @@ int main()
 		while (1)
 		{
 			cin>>m;
-			if (y<=g1[g1.size()-1].get_month())
-				cout<<"Incorrect month, input 1 more time: ";
-			else
+			if (check == true)
 				break;
+			else if (m<g1[g1.size()-1].get_month())
+				cout<<"Incorrect month, after: "<<g1[g1.size()-1].get_month()<< " ";
+			else if (m==g1[g1.size()-1].get_month())
+				{
+				check = false;
+				break;
+			}
+			else break;
 			}
 				
 				
@@ -145,8 +158,10 @@ int main()
 		while (1)
 		{
 			cin>>d;
-			if (y<=g1[g1.size()-1].get_day())
-				cout<<"Incorrect day, input 1 more time: ";
+			if (check == true)
+				break;
+			else if (d<=g1[g1.size()-1].get_day())
+				cout<<"Incorrect day, after: "<<g1[g1.size()-1].get_day()<< " ";
 			else
 				break;
 			}
@@ -168,13 +183,20 @@ int main()
 			
 	char des = 'y';
 	
+
+	
 	while (g1.size()>0 && des!='n')
 	{
+		
 		cout<<"Do u wanna kick smb from queue? (Y/N): ";
 		cin>>des;
 		if (des == 'y')
+		{
+			reverse(g1.begin(), g1.end());
 			g1.pop_back();
-		
+			reverse(g1.begin(), g1.end());
+		}
+			
 		cout<<endl;
 		for(int i = 0; i < g1.size(); i++)
 		{
